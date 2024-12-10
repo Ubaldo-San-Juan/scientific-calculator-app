e70c7a5c9bed044ace3c1687cafc47dc17abf5e7
Ubaldo-San-Juan <usan9fam@gmail.com>
2024-12-09 21:24:01 -0600

    Light mode added

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  public display: string = '222';
+
+  addValueToInput( value: string): void {
+    if(this.display.length < 30) this.display += value;
+  }
+
+  clearInput(): void {
+    this.display = '';
+  }
+
+  delete(): void {
+    this.display = this.display.slice(0, -1);
+  }
+
+  calculate(): void {
+    try {
+
+    } catch (error) {
+      this.display = 'Error';
+    }
+  }
 
 }
